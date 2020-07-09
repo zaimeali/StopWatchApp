6085cdc7a518f72f8436851017e1b8ba524ed016
zaime ali <zaimeali1997@gmail.com>
2020-07-10 01:24:56 +0500

    Done Stop Watch

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ let minID = 'minutes';
 let secID = 'seconds';
 let msecID = 'milliseconds';
 
-function timer(){
+let minute = 0;
+let second = 0;
+let millisecond = 0;
 
-}
+let timer;
 
 function init(){
     document.getElementById("stop").disabled = true;
@@ -16,6 +18,23 @@ function startWatch(){
     document.getElementById('start').disabled = true;
     document.getElementById("stop").disabled = false;
     document.getElementById("reset").disabled = false;
+
+    timer = setInterval(() => {
+        millisecond++;
+        document.getElementById(msecID).innerHTML = addZero(millisecond);
+        if(millisecond > 99){
+            second++;
+            millisecond = 0;
+            document.getElementById(secID).innerHTML = addZero(second);
+            console.log(second);
+        }
+        else if(second > 59){
+            minute++;
+            document.getElementById(minID).innerHTML = addZero(minute);
+            console.log(minute);
+            second = 0;
+        }
+    }, 10);
 }
 
 function stopWatch(){
@@ -23,6 +42,8 @@ function stopWatch(){
     document.getElementById('start').innerHTML = 'Resume <i class="fa fa-play" aria-hidden="true"></i>';
     document.getElementById("stop").disabled = true;
     document.getElementById("reset").disabled = false;
+
+    clearInterval(timer);
 }
 
 function resetWatch(){
@@ -30,4 +51,16 @@ function resetWatch(){
     document.getElementById('start').innerHTML = 'Start <i class="fa fa-play" aria-hidden="true"></i>';
     document.getElementById("stop").disabled = true;
     document.getElementById("reset").disabled = true;
+
+    document.getElementById(minID).innerHTML = "00";
+    document.getElementById(secID).innerHTML = "00";
+    document.getElementById(msecID).innerHTML = "00";
+
+    minute = 0;
+    second = 0;
+    millisecond = 0;
+}
+
+function addZero(time) {
+    return (time < 10) ? '0' + time.toString() : time.toString();
 }
